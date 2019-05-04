88d61890f0f76f6d5a21fdc77af9076215f38864
Zaid Albirawi <zalbiraw@gmail.com>
2019-05-04 12:32:30 -0400

    Upate App Structure

--- webapp/react/src/App/index.js
+++ webapp/react/src/App/index.js
@@ -1,49 +1,9 @@
 import React from 'react'
 
-import Header     from '../components/Header/index'
-import Profile    from '../components/Profile/index'
-import Education  from '../components/Education/index'
-import Skills     from '../components/Skills/index'
-import Experience from '../components/Experience/index'
-import Projects   from '../components/Projects/index'
-import Awards   	from '../components/Awards/index'
-import Interests  from '../components/Interests/index'
+import Resume from '../components/Resume/index'
 
 import './style.scss'
 
-const section = (component, title, className, icon) => (
-	<section className = {'row ' + className}>
-		<article className = 'column left'>
-			<div className = 'title'>
-				{ icon ? <i className = {'fas fa-' + icon} /> : null }
-				{title}
-			</div>
-		</article>
-		{component}
-	</section>
-)
-
-const App = () => (
-	<div className = 'resume-container'>
-		<div className = 'resume'>
-
-			{section(<Header />,     'Zaid Albirawi', 'header')}
-			{section(<Profile />,    'Profile',       'profile',    'user')}
-			{section(<Skills />,     'Skills',        'skills',     'star')}
-			{section(<Experience />, 'Experience',    'experience', 'briefcase')}
-			{section(<Education />,  'Education',     'education',  'graduation-cap')}
-			{section(<Projects />,   'Projects',      'projects',   'flask')}
-			{section(<Awards />,     'Awards',        'awards',     'trophy')}
-			{section(<Interests />,  'Interests',     'interests',  'futbol')}
-
-			<section className = 'row'>
-				<article className = 'column left'>
-				</article>
-				<article className = 'column right'>
-				</article>
-			</section>
-		</div>
-	</div>
-)
+const App = () => <Resume />
 
 export default App
