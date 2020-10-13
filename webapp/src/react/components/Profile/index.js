8dcddaf420b6200576b68588ffbfb424b69787f5
Zaid <zaid@osg.ca>
2020-10-13 12:19:35 -0400

    Update yrs of xp

--- webapp/src/react/components/Profile/index.js
+++ webapp/src/react/components/Profile/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <article className = 'column right'>
     <div className = 'summary'>
-      Detail-oriented software developer with 4+ years of experience
+      Detail-oriented software developer with 5+ years of experience
       in full stack development. Knowledgeable of high-performance
       computing, machine learning, and networking. Team player with
       a positive outlook to ensure optimal performance. Energetic,
